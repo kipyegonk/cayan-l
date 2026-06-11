931a66038f1bf34b097015360cbf0eada64ff609
kipyegonk <kevinlangat008@gmail.com>
2026-06-11 08:45:09 +0300

    Model Client

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -1,10 +1,12 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    protected $table    = 'clients';
+    protected $fillable = [
+        'name','email','phone',
+        'contact_person','location','address'
+    ];
 }
