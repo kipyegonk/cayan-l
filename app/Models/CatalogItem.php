16e459c5665062f0513d8082ac45c41e6c8c7b28
kipyegonk <kevinlangat008@gmail.com>
2026-06-11 08:43:12 +0300

    Model Catalog

--- app/Models/CatalogItem.php
+++ app/Models/CatalogItem.php
@@ -1,10 +1,17 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class CatalogItem extends Model
 {
-    //
+    protected $table    = 'catalog';
+    protected $fillable = [
+        'name','category','unit','description',
+        'cost_price','margin','unit_price'
+    ];
+    protected $casts = [
+        'cost_price' => 'float',
+        'margin'     => 'float',
+        'unit_price' => 'float',
+    ];
 }
