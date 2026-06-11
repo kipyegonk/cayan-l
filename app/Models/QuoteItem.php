fd59db464133cf126dc12ded7552e3e9f26ff4f0
kipyegonk <kevinlangat008@gmail.com>
2026-06-11 08:46:13 +0300

    Model quoteItem

--- app/Models/QuoteItem.php
+++ app/Models/QuoteItem.php
@@ -1,10 +1,17 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class QuoteItem extends Model
 {
-    //
+    protected $table    = 'quote_items';
+    protected $fillable = [
+        'quote_id','type','section','subsection',
+        'name','qty','unit_price','price','sort_order'
+    ];
+    protected $casts = [
+        'qty'        => 'float',
+        'unit_price' => 'float',
+        'price'      => 'float',
+    ];
 }
