12a51c6c0b245a41788c674dd42d0e27bec86c5c
kipyegonk <kevinlangat008@gmail.com>
2026-06-11 08:42:20 +0300

    Model Company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -1,10 +1,23 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $table    = 'company';
+    protected $fillable = [
+        'name','phone','email','address',
+        'currency','terms','logo','signature'
+    ];
+
+    public static function getSingleton(): self
+    {
+        return self::firstOrCreate(['id' => 1], [
+            'name'     => 'Cayan Events Ke.',
+            'phone'    => '0737 611 658',
+            'email'    => 'cayaneventsanddecor@gmail.com',
+            'address'  => 'Mokoyeti West Road, Karen',
+            'currency' => 'KES',
+        ]);
+    }
 }
