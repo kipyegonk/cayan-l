9641d4c6c381f8a51491982e19610b7f70e2849d
kipyegonk <kevinlangat008@gmail.com>
2026-06-12 09:33:54 +0300

    Company controller

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -1,10 +1,22 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Company;
 
 class CompanyController extends Controller
 {
-    //
+    public function show()
+    {
+        return response()->json(Company::getSingleton());
+    }
+
+    public function save(Request $request)
+    {
+        $company = Company::getSingleton();
+        $company->update($request->only([
+            'name','phone','email','address',
+            'currency','terms','logo','signature'
+        ]));
+        return response()->json(['success' => true]);
+    }
 }
