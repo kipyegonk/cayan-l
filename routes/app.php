3a72a1ca8cb4fa084dc89d16665c849283c4e413
kipyegonk <kevinlangat008@gmail.com>
2026-06-16 08:18:25 +0300

    routes app.php

---
+++ routes/app.php
@@ -0,0 +1,35 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\QuoteController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\StatsController;
+
+// Public
+Route::post('auth/login',    [AuthController::class, 'login']);
+Route::post('auth/register', [AuthController::class, 'register']);
+
+// Authenticated
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('auth/verify',           [AuthController::class, 'verify']);
+    Route::post('auth/change-password', [AuthController::class, 'changePassword']);
+
+    Route::get('company',  [CompanyController::class, 'show']);
+    Route::post('company', [CompanyController::class, 'save']);
+
+    Route::apiResource('catalog', CatalogController::class);
+    Route::apiResource('clients', ClientController::class);
+    Route::apiResource('quotes',  QuoteController::class);
+    Route::get('stats', [StatsController::class, 'index']);
+
+    // Admin only
+    Route::middleware('admin')->group(function () {
+        Route::get('users',           [UserController::class, 'index']);
+        Route::post('users',          [UserController::class, 'store']);
+        Route::put('users/{user}',    [UserController::class, 'update']);
+        Route::delete('users/{user}', [UserController::class, 'destroy']);
+    });
+});
