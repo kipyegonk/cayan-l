132947af2be0ba2c8085c38a1a5c676682354be0
kipyegonk <kevinlangat008@gmail.com>
2026-06-17 08:33:48 +0300

    routes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+$table->string('role', 20)->default('user');
+$table->boolean('verified')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
