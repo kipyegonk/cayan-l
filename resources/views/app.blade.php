2b7ee375f82b7260281bdd4a42f27bc6506699a3
kipyegonk <kevinlangat008@gmail.com>
2026-06-29 11:36:26 +0300

    Full mobile responsiveness - card layout on phone

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
 <title>Cayan Events Ke.</title>
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ filemtime(public_path('css/app.css')) }}">
 </head>
 <body>
 <div id="app"></div>
