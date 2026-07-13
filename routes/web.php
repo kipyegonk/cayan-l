e3ac9784fe21a486a6928eb1919b1b373c9d276a
kipyegonk <kevinlangat008@gmail.com>
2026-07-13 13:31:11 +0300

    app

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
+Route::get('login', function () {
+    return view('app');
+})->name('login');
+
 Route::get('/{any?}', function () {
     return view('app');
 })->where('any', '.*');
