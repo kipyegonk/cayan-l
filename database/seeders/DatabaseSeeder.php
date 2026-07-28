4d09730c38722e54fa8133297ea43b7940e24097
kipyegonk <kevinlangat008@gmail.com>
2026-07-28 09:08:57 +0300

    fix: password double-hashing in seeder due to model cast

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use App\Models\Company;
 
@@ -13,10 +12,15 @@ class DatabaseSeeder extends Seeder
             User::create([
                 'name'     => 'Admin',
                 'email'    => 'admin@company.com',
-                'password' => Hash::make('admin123'),
+                'password' => 'admin123', // hashed automatically by model cast
                 'role'     => 'admin',
                 'verified' => true,
             ]);
+        } else {
+            // Reset password in case of double-hash issue
+            $user = User::where('email','admin@company.com')->first();
+            $user->password = 'admin123';
+            $user->save();
         }
         Company::getSingleton();
         $this->command->info('Done! Login: admin@company.com / admin123');
